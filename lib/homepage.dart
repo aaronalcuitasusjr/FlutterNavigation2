3ce08b1c3eca9e8e1a7f90dcef42ce4bc411f3fb
aaronalcuitasusjr <aaronbenjmin.alcuitas.20@usjr.edu.ph>
2022-10-24 01:27:09 +0800

    improved UI for homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,76 +11,362 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: Colors.black,
       body: SafeArea(
         child: Container(
-          padding:
-              EdgeInsets.only(left: 20.0, top: 40.0, right: 20.0, bottom: 40.0),
-          color: Colors.white,
+          padding: EdgeInsets.only(top: 20.0, bottom: 12.0),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              alignment: Alignment.topCenter,
+              image: AssetImage('assets/images/background.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
           alignment: Alignment.center,
           child: Column(children: [
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              Text(
-                'My Music',
-                style: TextStyle(fontFamily: 'Roboto', fontSize: 32),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size(100, 40),
-                  backgroundColor: Colors.orange,
-                  elevation: 4.0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16.0),
+            Container(
+              padding: EdgeInsets.only(left: 20.0, right: 20.0),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 32,
+                            color: Colors.white),
+                        children: const [
+                          TextSpan(
+                              text: 'User',
+                              style: TextStyle(color: Colors.orange)),
+                          TextSpan(text: '\'s Music'),
+                        ],
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(100, 40),
+                        backgroundColor: Colors.orange,
+                        elevation: 4.0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                      ),
+                      child: Text('Log Out'),
+                    )
+                  ]),
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 20,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 20.0, right: 20.0),
+              child: TextField(
+                style: TextStyle(
+                  color: Colors.purpleAccent,
+                ),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(32.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(32.0),
+                    borderSide: BorderSide(
+                      color: Colors.purple,
+                    ),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                  labelText: 'Search',
+                  hintText: 'Enter any title, artist, or album',
+                  labelStyle: TextStyle(
+                    color: Colors.purpleAccent,
+                  ),
+                  hintStyle: TextStyle(
+                    color: Colors.deepPurple,
                   ),
                 ),
-                child: Text('Log Out'),
-              )
-            ]),
+              ),
+            ),
             SizedBox(
               width: double.infinity,
               height: 20,
             ),
             Expanded(
               child: Container(
-                padding: EdgeInsets.only(left: 8.0, top: 8.0, right: 8.0),
                 decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.lightBlue,
-                    ),
-                    color: Colors.lightBlue,
-                    borderRadius: BorderRadius.all(Radius.circular(16))),
-                child: ListView(children: const [
-                  MusicItem(title: 'Moscow Mule', artist: 'Bad Bunny'),
-                  MusicItem(title: 'Después de la Playa', artist: 'Bad Bunny'),
-                  MusicItem(
-                      title: 'Me Porto Bonito',
-                      artist: 'Bad Bunny, Chencho Corleone'),
-                  MusicItem(title: 'Tití Me Preguntó', artist: 'Bad Bunny'),
-                  MusicItem(title: 'Un Ratito', artist: 'Bad Bunny'),
-                  MusicItem(title: 'Yo No Soy Celoso', artist: 'Bad Bunny'),
-                  MusicItem(title: 'Tarot', artist: 'Bad Bunny, Jhayco'),
-                  MusicItem(title: 'Neverita', artist: 'Bad Bunny'),
-                ]),
+                  color: Colors.white,
+                ),
+                child: SingleChildScrollView(
+                  padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: double.infinity,
+                        height: 20,
+                      ),
+                      Text(
+                        'Un Verano Sin Ti',
+                        style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 24,
+                            color: Colors.purple),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        margin: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                        padding:
+                            EdgeInsets.only(left: 8.0, top: 8.0, right: 8.0),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(
+                              color: Colors.lightBlue,
+                            ),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(16))),
+                        child: Column(children: [
+                          MusicItem(
+                              title: 'Moscow Mule',
+                              artist: 'Bad Bunny',
+                              picture: 'assets/images/album_icon.png',
+                              isPlaying: true),
+                          MusicItem(
+                              title: 'Después de la Playa',
+                              artist: 'Bad Bunny',
+                              picture: 'assets/images/album_icon.png',
+                              isPlaying: false),
+                          MusicItem(
+                              title: 'Me Porto Bonito',
+                              artist: 'Bad Bunny, Chencho Corleone',
+                              picture: 'assets/images/album_icon.png',
+                              isPlaying: false),
+                          ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                minimumSize: Size(double.infinity, 40),
+                                backgroundColor: Colors.lightBlue,
+                                elevation: 0,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16.0),
+                                ),
+                              ),
+                              child: Text('View All')),
+                        ]),
+                      ),
+                      Text(
+                        'BORN PINK',
+                        style: TextStyle(
+                          fontFamily: 'Roboto',
+                          fontSize: 24,
+                          color: Colors.purple,
+                        ),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        margin: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                        padding:
+                            EdgeInsets.only(left: 8.0, top: 8.0, right: 8.0),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(
+                              color: Colors.lightBlue,
+                            ),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(16))),
+                        child: Column(children: [
+                          MusicItem(
+                              title: 'Pink Venom',
+                              artist: 'BLACKPINK',
+                              picture: 'assets/images/album_icon_2.jpeg',
+                              isPlaying: false),
+                          MusicItem(
+                              title: 'Shut Down',
+                              artist: 'BLACKPINK',
+                              picture: 'assets/images/album_icon_2.jpeg',
+                              isPlaying: false),
+                          MusicItem(
+                              title: 'Typa Girl',
+                              artist: 'BLACKPINK',
+                              picture: 'assets/images/album_icon_2.jpeg',
+                              isPlaying: false),
+                          ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                minimumSize: Size(double.infinity, 40),
+                                backgroundColor: Colors.lightBlue,
+                                elevation: 0,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16.0),
+                                ),
+                              ),
+                              child: Text('View All')),
+                        ]),
+                      ),
+                      Text(
+                        'Dawn FM',
+                        style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 24,
+                            color: Colors.purple),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        margin: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                        padding:
+                            EdgeInsets.only(left: 8.0, top: 8.0, right: 8.0),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(
+                              color: Colors.lightBlue,
+                            ),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(16))),
+                        child: Column(children: [
+                          MusicItem(
+                              title: 'Dawn FM',
+                              artist: 'The Weeknd',
+                              picture: 'assets/images/album_icon_3.png',
+                              isPlaying: false),
+                          MusicItem(
+                              title: 'Gasoline',
+                              artist: 'The Weeknd',
+                              picture: 'assets/images/album_icon_3.png',
+                              isPlaying: false),
+                          MusicItem(
+                              title: 'How Do I Make You..',
+                              artist: 'The Weeknd',
+                              picture: 'assets/images/album_icon_3.png',
+                              isPlaying: false),
+                          ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                minimumSize: Size(double.infinity, 40),
+                                backgroundColor: Colors.lightBlue,
+                                elevation: 0,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16.0),
+                                ),
+                              ),
+                              child: Text('View All')),
+                        ]),
+                      ),
+                      Text(
+                        'My new playlist',
+                        style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 24,
+                            color: Colors.purple),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        margin: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                        padding:
+                            EdgeInsets.only(left: 8.0, top: 8.0, right: 8.0),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(
+                              color: Colors.lightBlue,
+                            ),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(16))),
+                        child: Column(children: [
+                          MusicItem(
+                              title: 'New To You',
+                              artist: 'Calvin Harris, Normani, Tinashe, Offset',
+                              picture: 'assets/images/album_icon_4.jpeg',
+                              isPlaying: false),
+                          MusicItem(
+                              title: 'Obsessed',
+                              artist: 'Calvin Harris, Charlie Puth, Shenseea',
+                              picture: 'assets/images/album_icon_4.jpeg',
+                              isPlaying: false),
+                          MusicItem(
+                              title: 'If You Let Me',
+                              artist: 'Alina Baraz',
+                              picture: 'assets/images/album_icon_5.jpg',
+                              isPlaying: false),
+                          ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                minimumSize: Size(double.infinity, 40),
+                                backgroundColor: Colors.lightBlue,
+                                elevation: 0,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16.0),
+                                ),
+                              ),
+                              child: Text('View All')),
+                        ]),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
             SizedBox(
               width: double.infinity,
-              height: 20,
+              height: 12,
             ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const FigureA()));
-                },
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size(double.infinity, 40),
-                  backgroundColor: Colors.purpleAccent,
-                  elevation: 4.0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16.0),
+            Container(
+              padding: EdgeInsets.only(left: 20.0, right: 20.0),
+              child: Row(children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 40,
+                        width: 40,
+                        margin: EdgeInsets.only(right: 8.0),
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            alignment: Alignment.centerLeft,
+                            image: AssetImage('assets/images/album_icon.png'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Bad Bunny - Moscow Mule',
+                              style: TextStyle(color: Colors.white)),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Container(
+                            height: 8,
+                            width: 168,
+                            alignment: Alignment.centerLeft,
+                            child: LinearProgressIndicator(
+                              value: 0.34,
+                              backgroundColor: Colors.white,
+                              color: Colors.orange,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-                child: Text('View All')),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const FigureA()));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(100, 40),
+                      backgroundColor: Colors.purpleAccent,
+                      elevation: 4.0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                    ),
+                    child: Text('Add Playlist')),
+              ]),
+            ),
           ]),
         ),
       ),
--- lib/music_item.dart
+++ lib/music_item.dart
@@ -1,65 +1,139 @@
 import 'package:flutter/material.dart';
 
 class MusicItem extends StatelessWidget {
-  const MusicItem({super.key, required this.title, required this.artist});
+  const MusicItem(
+      {super.key,
+      required this.title,
+      required this.artist,
+      required this.picture,
+      required this.isPlaying});
 
   final String title;
   final String artist;
+  final String picture;
+  final bool isPlaying;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 80,
-      margin: EdgeInsets.only(bottom: 8.0),
-      padding: EdgeInsets.only(right: 4.0),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(16))),
-      child: Row(
-        children: [
-          Container(
-            height: 80,
-            width: 80,
-            margin: EdgeInsets.only(right: 8.0),
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                  alignment: Alignment.centerLeft,
-                  image: AssetImage('assets/images/album_icon.png'),
-                  fit: BoxFit.cover,
-                ),
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(16),
-                    bottomLeft: Radius.circular(16))),
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  title,
-                  style: TextStyle(
-                      fontFamily: 'Roboto',
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold),
-                ),
-                Text(artist, style: TextStyle(fontSize: 12)),
-              ],
+    if (isPlaying == true) {
+      return Container(
+        width: double.infinity,
+        height: 80,
+        margin: EdgeInsets.only(bottom: 8.0),
+        padding: EdgeInsets.only(right: 4.0),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              color: Colors.lightBlue,
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              fixedSize: const Size(40, 40),
-              backgroundColor: Colors.purpleAccent,
-              shape: const CircleBorder(),
+            borderRadius: BorderRadius.all(Radius.circular(16))),
+        child: Row(
+          children: [
+            Container(
+              height: 80,
+              width: 80,
+              margin: EdgeInsets.only(right: 8.0),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                    alignment: Alignment.centerLeft,
+                    image: AssetImage(picture),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16),
+                      bottomLeft: Radius.circular(16))),
             ),
-            child: Icon(Icons.play_arrow),
-          ),
-        ],
-      ),
-    );
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.orange[700]),
+                  ),
+                  Text(artist,
+                      style:
+                          TextStyle(fontSize: 12, color: Colors.orange[300])),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                fixedSize: const Size(40, 40),
+                backgroundColor: Colors.orange,
+                shape: const CircleBorder(),
+              ),
+              child: Icon(Icons.pause_rounded),
+            ),
+          ],
+        ),
+      );
+    } else {
+      return Container(
+        width: double.infinity,
+        height: 80,
+        margin: EdgeInsets.only(bottom: 8.0),
+        padding: EdgeInsets.only(right: 4.0),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              color: Colors.lightBlue,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(16))),
+        child: Row(
+          children: [
+            Container(
+              height: 80,
+              width: 80,
+              margin: EdgeInsets.only(right: 8.0),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                    alignment: Alignment.centerLeft,
+                    image: AssetImage(picture),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16),
+                      bottomLeft: Radius.circular(16))),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue[700]),
+                  ),
+                  Text(artist,
+                      style: TextStyle(
+                          fontSize: 12, color: Colors.lightBlue[300])),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                fixedSize: const Size(40, 40),
+                backgroundColor: Colors.lightBlue,
+                shape: const CircleBorder(),
+              ),
+              child: Icon(Icons.play_arrow),
+            ),
+          ],
+        ),
+      );
+    }
   }
 }
